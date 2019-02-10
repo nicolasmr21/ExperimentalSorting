291c9cb6dc51875b338ae6ec79287827a26e95ed
Nicolas Martinez-Juan Manuel Castillo <nicolasmartinez502@gmail.com>
2019-02-10 18:26:11 -0500

    final

--- ExperimentalSorting/model/Experiment.cs
+++ ExperimentalSorting/model/Experiment.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ExperimentalSorting.Model
 {
-<<<<<<< HEAD
-    class Experiment
-=======
     public class Experiment
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
     { 
         public int[] generate(int a, int b)
         {
@@ -43,57 +39,37 @@ namespace ExperimentalSorting.Model
         }
         public int[] generateBigArrayRandomOrder() {
             Random r = new Random();
-<<<<<<< HEAD
             return Enumerable.Repeat(0, 10000000).Select(x => r.Next(1, 100000000)).ToArray();
-=======
-            return Enumerable.Repeat(0, 1000000).Select(x => r.Next(1, 1000000)).ToArray();
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
+
         }
 
         public int[] generateSmallArrayRandomOrder()
         {
             Random r = new Random();
-<<<<<<< HEAD
             return Enumerable.Repeat(0, 100000).Select(x => r.Next(1, 1000000)).ToArray();
-=======
-            return Enumerable.Repeat(0, 1000).Select(x => r.Next(1, 1000000)).ToArray();
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
         }
 
         public int[] generateBigArrayAscendenntOrder()
         {
-<<<<<<< HEAD
             return Enumerable.Range(1, 10000000).ToArray();
-=======
-            return Enumerable.Range(1, 1000000).ToArray();
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
         }
 
         public int[] generateSmallArrayAscendentOrder()
         {
-<<<<<<< HEAD
             return Enumerable.Range(1, 100000).ToArray();
-=======
-            return Enumerable.Range(1, 1000).ToArray();
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
+
         }
 
         public int[] generateBigArrayDescendentOrder()
         {
-<<<<<<< HEAD
             return Enumerable.Range(1, 10000000).Reverse().ToArray();
-=======
-            return Enumerable.Range(1, 1000000).Reverse().ToArray();
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
+
         }
 
         public int[] generateSmallArrayDescendentOrder()
         {
-<<<<<<< HEAD
             return Enumerable.Range(1, 100000).Reverse().ToArray();
-=======
-            return Enumerable.Range(1, 1000).Reverse().ToArray();
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
+
         }
 
     }
--- ExperimentalSorting/view/App.cs
+++ ExperimentalSorting/view/App.cs
@@ -24,10 +24,7 @@ namespace ExperimentalSorting
         {
             Treatment t = new Treatment(new Experiment().generate(comboState.SelectedIndex, comboSize.SelectedIndex), comboAlgorithm.SelectedItem.Equals("Counting Sort"));
             t.orderArray();
-<<<<<<< HEAD
             Console.WriteLine("------------");
-=======
->>>>>>> 50481a2110f77be0d534a5b8b1dde6b94e4c909b
         }
 
         private void comboAlgorithm_SelectedIndexChanged(object sender, EventArgs e)
